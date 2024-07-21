9b4f86e50e05a5e182edca7bbfc996a2ac4b2d52
Luiz Bills <luizbills@pm.me>
2024-07-21 14:05:32 -0300

    catch any kind of error

--- src/template.js
+++ src/template.js
@@ -48,13 +48,9 @@ export default function template(library, game) {
       <p id="err-tip">Tip: If you are loading any asset (script, font, image etc), don't forget to add <code>if (LOADING > 0) return;</code> at the beginning of your <code>update</code> and <code>draw</code> functions.</p>
     </div>
     <script>
-      // catch errors
-      const err = document.getElementById('err')
-      window.addEventListener('error', (ev) => {
-        console.error(ev)
-        err.prepend(ev.message)
-        err.style.display = 'block'
-      })
+      // blue screen of death
+      handleError=e=>{console.log(e),err.prepend(e.message||e.reason),err.style.display='block'};
+      addEventListener('error',handleError);addEventListener('unhandledrejection',handleError);
     </script>
     <script>${library}</script>
     <script>
