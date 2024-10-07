340ce7eff2eb679c0241c112655cc9f4228aa092
Luiz Bills <luizbills@pm.me>
2024-10-06 22:25:28 -0300

    improve mobile bar

--- src/mobileBar.js
+++ src/mobileBar.js
@@ -1,5 +1,11 @@
-import { toggleComment, undo, redo, indentMore } from "@codemirror/commands";
-// import { openSearchPanel } from "@codemirror/search";
+import {
+  toggleComment,
+  undo,
+  redo,
+  indentMore,
+  selectAll,
+} from "@codemirror/commands";
+import { openSearchPanel } from "@codemirror/search";
 
 export default function mobileBar(editorView) {
   const viewport = visualViewport;
@@ -29,12 +35,17 @@ export default function mobileBar(editorView) {
       label: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m15 15 6-6m0 0-6-6m6 6H9a6 6 0 0 0 0 12h3" /></svg>`,
       callback: redo,
     },
-    // {
-    //   name: "search (and replace)",
-    //   label: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>`,
-    //   callback: openSearchPanel,
-    //   focus: false,
-    // },
+    {
+      name: "select all",
+      label: `[A]`,
+      callback: selectAll,
+    },
+    {
+      name: "search (and replace)",
+      label: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" /></svg>`,
+      callback: openSearchPanel,
+      focus: false,
+    },
   ];
 
   const buttons = document.createElement("div");
