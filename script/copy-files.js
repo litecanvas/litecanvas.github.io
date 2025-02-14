09e05e658291f9f4fa793932a0b603623c8d180e
Luiz Bills <luizbills@pm.me>
2025-02-13 21:12:56 -0300

    update engine

--- script/copy-files.js
+++ script/copy-files.js
@@ -1,11 +1,12 @@
 import { copyFile, unlink, readFile, appendFile } from "node:fs/promises";
+import esbuild from "esbuild";
 
 const root = process.env["PWD"];
 const engineFile = root + "/public/litecanvas.js";
 
 await unlink(engineFile);
 
-await copyFile(root + "/node_modules/litecanvas/dist/dist.min.js", engineFile);
+await copyFile(root + "/node_modules/litecanvas/dist/dist.dev.js", engineFile);
 
 const utils = await readFile(
   root + "/node_modules/@litecanvas/utils/dist/all.min.js",
@@ -20,3 +21,12 @@ const pluginAssetLoader = await readFile(
 );
 
 await appendFile(engineFile, "\n" + pluginAssetLoader);
+
+await esbuild.build({
+  entryPoints: [engineFile],
+  outfile: engineFile,
+  bundle: true,
+  minify: true,
+  legalComments: "eof",
+  allowOverwrite: true,
+});
