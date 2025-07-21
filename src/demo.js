5aad1975137365bd5febc0bd5e580bd25516e49e
Luiz Bills <luizbills@pm.me>
2025-07-21 17:01:02 -0300

    fix demo identation

--- src/demo.js
+++ src/demo.js
@@ -5,38 +5,39 @@ export default () =>
 
 // Start the engine
 litecanvas({
-    loop: { init, update, draw, tapped },
+  loop: { init, update, draw, tapped },
+  pixelart: true
 })
 
 let bg, color, radius, posx, posy
 
 // this function runs once at the beginning
 function init() {
-    bg = 0 // the color #0 (black)
-    color = 3 // the color #3 (white)
-    radius = W / 10 // canvas.width / 10
-    posx = W / 2 // center X or canvas.width / 2
-    posy = H / 2 // center Y or canvas.height / 2
+  bg = 0 // the color #0 (black)
+  color = 3 // the color #3 (white)
+  radius = W / 10 // canvas.width / 10
+  posx = W / 2 // center X or canvas.width / 2
+  posy = H / 2 // center Y or canvas.height / 2
 }
 
 // this function detect clicks/touches
 function tapped(x, y) {
-    // changes the circle position
-    // based on the position of the tap
-    posx = x
-    posy = y
+  // changes the circle position
+  // based on the position of the tap
+  posx = x
+  posy = y
 }
 
 // put the game logic in this function
 function update(dt) {
-    // make the circle falls 200 pixels per second
-    posy += 200 * dt
+  // make the circle falls 200 pixels per second
+  posy += 200 * dt
 }
 
 // put the game rendering in this function
 function draw() {
-    cls(bg) // clear the screen
-    circfill(posx, posy, radius, color) // draw a circle
-    text(10, 10, 'Tap anywhere') // draw a text
+  cls(bg) // clear the screen
+  circfill(posx, posy, radius, color) // draw a circle
+  text(10, 10, 'Tap anywhere') // draw a text
 }
 `;
